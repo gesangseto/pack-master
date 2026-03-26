c96b106b267be774ca7c67436a6a27755fc6485d
MMDS-LT02\mmdsd <gesang@merindo.co.id>
2026-03-26 09:39:39 +0700

    Mencoba mencockan layout dengan aslinya

--- src/component/Panel.jsx
+++ src/component/Panel.jsx
@@ -1,63 +1,159 @@
 import React from 'react';
-import { Box, Grid, Typography, Paper, Button, TextField } from '@mui/material';
+import { styled } from '@mui/material/styles';
+
+import {
+  Box,
+  Stack,
+  Grid,
+  Typography,
+  Paper,
+  Button,
+  TextField,
+} from '@mui/material';
+import CenterFocusStrongIcon from '@mui/icons-material/CenterFocusStrong';
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: (theme.vars ?? theme).palette.text.secondary,
+  ...theme.applyStyles('dark', {
+    backgroundColor: '#1A2027',
+  }),
+}));
 
 export default function Panel(props) {
   const { title } = props;
   return (
-    <Box p={2} bgcolor="#e6e6e6" minHeight="100vh">
-      {/* HEADER */}
-      <Box
-        display="flex"
-        alignItems="center"
-        justifyContent="space-between"
-        bgcolor="#0d1fa6"
-        color="white"
-        px={2}
-        py={1}
-        borderRadius={2}
-      >
-        <Typography variant="h6">Level 2</Typography>
-        <Typography variant="h5" fontWeight="bold">
-          {title}
-        </Typography>
-        <Button variant="contained" disabled>
-          Login
-        </Button>
+    <Paper sx={{ p: 2 }} bgcolor="#e6e6e6" minHeight="100vh">
+      {/* TOP PANEL */}
+      <Box display="flex" gap={1}>
+        {/* TOP LEFT PANEL */}
+        <Box
+          flex={1}
+          display="flex"
+          alignItems="center"
+          justifyContent="space-between"
+          bgcolor="#0d1fa6"
+          color="white"
+          px={3}
+          borderRadius={3}
+        >
+          <Box textAlign="center">
+            <Typography variant="caption">LEVEL</Typography>
+            <Typography variant="h6" fontWeight="bold">
+              2
+            </Typography>
+          </Box>
+          <Typography variant="h5" fontWeight="bold">
+            Master Box
+          </Typography>
+          <Typography variant="h5" fontWeight="bold">
+            {title}
+          </Typography>
+        </Box>
+        {/* TOP RIGHT PANEL */}
+        <Box
+          flex={1}
+          display="flex"
+          alignItems="center"
+          justifyContent="space-between"
+          bgcolor="#8a8a8a"
+          color="white"
+          px={3}
+          borderRadius={3}
+        >
+          {/* LOGIN BUTTON */}
+          <Button
+            variant="contained"
+            color="success"
+            sx={{ borderRadius: 2, px: 3 }}
+            disabled
+          >
+            Login
+          </Button>
+
+          <Typography variant="h5">Operator 1</Typography>
+          {/* LEVEL */}
+          <Box textAlign="center">
+            <Typography variant="caption">Auto Logout id:</Typography>
+            <Typography variant="h6" fontWeight="bold">
+              12000
+            </Typography>
+          </Box>
+        </Box>
       </Box>
 
-      <Grid container spacing={2} mt={1}>
-        {/* LEFT PANEL */}
-        <Grid item xs={8}>
-          <Paper sx={{ p: 2 }}>
-            <Box bgcolor="#666" color="white" textAlign="center" py={2} mb={2}>
-              <Typography variant="h6">Silakan login panel A</Typography>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={1}>
+          {/* LEFT PANEL */}
+          <Grid size={8}>
+            <Box p={2}>
+              <Grid container spacing={1}>
+                <Grid
+                  size={2}
+                  sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                  }}
+                >
+                  <Button>
+                    <CenterFocusStrongIcon fontSize="large" />
+                  </Button>
+                </Grid>
+                <Grid
+                  size={8}
+                  sx={{
+                    borderRadius: 3,
+                    bgcolor: 'red',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    color: 'error.contrastText',
+                  }}
+                >
+                  <Typography variant="h6" textAlign="center">
+                    Silakan login {title}
+                  </Typography>
+                </Grid>
+                <Grid size={2}>
+                  <Stack spacing={2} alignItems="center">
+                    <Button>
+                      <AddIcon />
+                    </Button>
+                    <Button>
+                      <RemoveIcon />
+                    </Button>
+                  </Stack>
+                </Grid>
+              </Grid>
             </Box>
+            <Box>
+              <Typography variant="h6" mb={1}>
+                DAFTAR EPC ISI ()
+              </Typography>
+              <Box
+                height={300}
+                border="1px solid #ccc"
+                borderRadius={2}
+                bgcolor="#fafafa"
+              />
+              <Box display="flex" justifyContent="space-between" mt={2}>
+                <Button variant="contained" color="error">
+                  Close Partial
+                </Button>
 
-            <Typography variant="h6" mb={1}>
-              DAFTAR EPC ISI ()
-            </Typography>
+                <Button variant="contained">Print Label</Button>
+              </Box>
+            </Box>
+          </Grid>
 
-            <Box
-              height={300}
-              border="1px solid #ccc"
-              borderRadius={2}
-              bgcolor="#fafafa"
-            />
-          </Paper>
-
-          {/* FOOTER BUTTON */}
-          <Box display="flex" justifyContent="space-between" mt={2}>
-            <Button variant="contained" color="error">
-              Close Partial
-            </Button>
-
-            <Button variant="contained">Print Label</Button>
-          </Box>
-        </Grid>
-
-        {/* RIGHT PANEL */}
-        <Grid item xs={4}>
-          <Paper sx={{ p: 2 }}>
+          {/* RIGHT PANEL */}
+          <Grid size={4} pt={2}>
             {/* Progress */}
             <Typography textAlign="center" mb={1}>
               Progres berjalan
@@ -73,39 +169,41 @@ export default function Panel(props) {
               </Box>
             </Box>
 
-            {/* Kriteria */}
-            <Typography>Kriteria Bobot</Typography>
-            <Typography>Average 0 | 0/0</Typography>
+            <Box p={1} border="1px solid #ccc" borderRadius={2}>
+              {/* Kriteria */}
+              <Typography>Kriteria Bobot</Typography>
+              <Typography>Average 0 | 0/0</Typography>
 
-            {/* Range */}
-            <Typography mt={2}>Rentang Bobot (kg)</Typography>
-            <Box display="flex" gap={1} alignItems="center" mt={1}>
-              <TextField size="small" label="Min" fullWidth />
-              <Typography>~</Typography>
-              <TextField size="small" label="Max" fullWidth />
+              {/* Range */}
+              <Typography mt={2}>Rentang Bobot (kg)</Typography>
+              <Box display="flex" gap={1} alignItems="center" mt={1}>
+                <TextField size="small" label="Min" fullWidth />
+                <Typography>~</Typography>
+                <TextField size="small" label="Max" fullWidth />
+              </Box>
+
+              {/* Timbang */}
+              <Button fullWidth variant="contained" sx={{ mt: 2 }} disabled>
+                Timbang
+              </Button>
+
+              {/* Result */}
+              <Typography mt={2}>Hasil Timbang ()</Typography>
+              <Box
+                bgcolor="#eee"
+                textAlign="center"
+                py={2}
+                mt={1}
+                borderRadius={1}
+              >
+                <Typography variant="h4" color="blue">
+                  0.000
+                </Typography>
+              </Box>
             </Box>
-
-            {/* Timbang */}
-            <Button fullWidth variant="contained" sx={{ mt: 2 }} disabled>
-              Timbang
-            </Button>
-
-            {/* Result */}
-            <Typography mt={2}>Hasil Timbang ()</Typography>
-            <Box
-              bgcolor="#eee"
-              textAlign="center"
-              py={2}
-              mt={1}
-              borderRadius={1}
-            >
-              <Typography variant="h4" color="blue">
-                0.000
-              </Typography>
-            </Box>
-          </Paper>
+          </Grid>
         </Grid>
-      </Grid>
-    </Box>
+      </Box>
+    </Paper>
   );
 }
